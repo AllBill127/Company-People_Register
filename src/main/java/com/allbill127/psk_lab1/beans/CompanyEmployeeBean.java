9fe70493f4fe47727c9ecab946240313a5625a36
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-04-17 00:44:34 +0300

    add Empoyee entity addition form
    add employee addition form and bean to manage the data
    note: does not refresh employee list although redirect seems to happen

--- src/main/java/com/allbill127/psk_lab1/beans/CompanyEmployeeBean.java
+++ src/main/java/com/allbill127/psk_lab1/beans/CompanyEmployeeBean.java
@@ -1,47 +1,68 @@
 package com.allbill127.psk_lab1.beans;
 
-import com.allbill127.psk_lab1.entities.Company;
 import com.allbill127.psk_lab1.JPA_DAO.CompanyDAO;
-import com.allbill127.psk_lab1.entities.Employee;
 import com.allbill127.psk_lab1.JPA_DAO.EmployeeDAO;
+import com.allbill127.psk_lab1.JPA_DAO.PersonDAO;
+import com.allbill127.psk_lab1.entities.Company;
+import com.allbill127.psk_lab1.entities.Employee;
+import com.allbill127.psk_lab1.entities.Person;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.lang.Long.parseLong;
 
 @Model
 public class CompanyEmployeeBean {
+    private static final Logger _logger = Logger.getLogger(CompanyEmployeeBean.class.getName());
     @Inject
     private CompanyDAO companyDAO;
-
+    @Inject
+    private PersonDAO personDAO;
     @Inject
     private EmployeeDAO employeeDAO;
 
     @Getter @Setter
-    private Company company;
-
+    private String personIdString = new String();
     @Getter @Setter
-    private Employee employeeToCreate = new Employee();
-
-    @PostConstruct
-    public void init(){
-        Map<String, String> requestParameters =
-                FacesContext
-                .getCurrentInstance()
-                .getExternalContext()
-                .getRequestParameterMap();
-        long companyId = Long.parseLong(requestParameters.get("companyId"));
-        this.company = companyDAO.getById(companyId);
-    }
+    private String companyIdString = new String();
+    @Getter @Setter
+    private Employee employee = new Employee();
 
     @Transactional
-    public void createEmployee(){
-        employeeToCreate.setCompany(this.company);
-        employeeDAO.create(employeeToCreate);
+    public String createEmployee() {
+        Object[] logParams = {this.getClass().getName(),
+                companyIdString,
+                personIdString};
+        _logger.log(Level.WARNING, "CompanyEmployeeBean companyIdString={1} personIdString={2}",
+                logParams);
+
+        if(companyIdString == "" || companyIdString == null){
+            _logger.log(Level.WARNING, "CompanyEmployeeBean companyIdString = null/empty");
+            return "companyDetails.xhtml";
+        }
+        if(personIdString == "" || personIdString == null){
+            _logger.log(Level.WARNING, "CompanyEmployeeBean personIdString = null/empty");
+            return "companyDetails.xhtml";
+        }
+
+        long companyId = parseLong(companyIdString);
+        long personId = parseLong(personIdString);
+        Company company = this.companyDAO.getById(companyId);
+        Person person = this.personDAO.getById(personId);
+
+        this.employee.setCompany(company);
+        this.employee.setPerson(person);
+        this.employeeDAO.create(employee);
+
+        //TODO:
+        // force reload page. currently it inserts data to DB and selects the data
+        // for the page but the employee list does not update
+        return "companyDetails.xhtml?face-redirect=true";
     }
 }
